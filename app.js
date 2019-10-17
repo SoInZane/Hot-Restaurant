ea06f88ed91dd97bee9d1e4c09f281b58060a1fd
bradham <bradham@users.noreply.github.com>
2019-10-16 21:28:22 -0500

    Api calls post implemented

--- app.js
+++ app.js
@@ -12,22 +12,63 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Global array for holding tables and reservations
+var tablesArr = [
+    {
+        "customerName": "dummy",
+        "phoneNumber": "555-2345",
+        "customerEmail": "fake@gmail.com",
+        "customerID": "1"
+        }
+];
+var waitListArr = [
+    {
+    "customerName": "dummy2",
+    "phoneNumber": "2555-2345",
+    "customerEmail": "fake@gmail.com",
+    "customerID": "1"
+    }
+
+];
+
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
-  app.get("/table", function(req, res) {
+});
+
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-  });
+});
 
-  app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Displays tables api
+app.get("/api/tables", function(req, res) {
+    return res.json(tablesArr);
   });
 
-  // Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  // Displays tables api
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitListArr);
   });
-  
+
+  app.post("/api/tables", function(req, res) {
+      var newTable = req.body;
+
+      if (tablesArr.length < 5) {
+      tablesArr.push(newTable);
+      } else {
+          waitListArr.push(newTable);
+      }
+
+      res.json(newTable);
+  });
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
